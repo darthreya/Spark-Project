a4843ed78ea60561f9c9b77acf2cb41fca56ff14
darthreya <athreya@umich.edu>
2016-08-01 18:14:31 -0400

    edited

--- firmware/Adafruit_SHT31.h
+++ firmware/Adafruit_SHT31.h
@@ -14,8 +14,17 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
-#if (ARDUINO >= 100)
- #include "Arduino.h"
+#if defined (SPARK)
+  #include "application.h"
+#else
+  #if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+  // here could follow some of the includes only needed on Arduino
+  // see bellow
+
 #endif
 #include "Wire.h"
 
